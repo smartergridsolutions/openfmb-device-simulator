2454e63ac9d4b14a885e9cd79a3338fdfb9c748b
Garret Fick <gfick@smartergridsolutions.com>
2019-08-16 10:18:06 -0400

    OFMBDS-18 Address copy-paste error in the module documentation

--- openfmbsim/devices/single_phase_recloser.py
+++ openfmbsim/devices/single_phase_recloser.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Simple simulator for a battery generation unit."""
+"""Simple simulator for a recloser unit."""
 
 from datetime import datetime
 import uuid
